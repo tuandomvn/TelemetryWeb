7222a9e289a7b7aa76818077026b40c5724234ec
tuandomvn <tuandomvn@gmail.com>
2026-03-23 22:07:16 +0700

    abc

--- TelemetryWeb/Telemetry/AppTodaySummary.cs
+++ TelemetryWeb/Telemetry/AppTodaySummary.cs
@@ -3,6 +3,6 @@ namespace TelemetryWeb.Telemetry;
 public sealed record AppTodaySummary(
     string App,
     int LogsToday,
-    DateTimeOffset? LastLogUtc,
+    DateTime? LastLogUtc,
     bool IsIdle);
 
